4f2907b4cf4351d02f029fd7e68fb48c664542d4
Arsen Petrosian <xckaytz@gmail.com>
2025-11-14 01:58:12 +0200

    updated tokenURL to twitter auth

--- src/modules/auth/strategies/twitter.strategy.ts
+++ src/modules/auth/strategies/twitter.strategy.ts
@@ -21,6 +21,7 @@ export class TwitterStrategy extends PassportStrategy(Strategy, 'twitter') {
       state: 'state',
       clientType: 'confidential',
       authorizationURL: 'https://x.com/i/oauth2/authorize',
+      tokenURL: 'https://api.x.com/2/oauth2/token',
     });
   }
 
