d15fa48a19a1a1453e6094c7ad1760e0896a8660
Arsen Petrosian <xckaytz@gmail.com>
2025-11-14 00:33:55 +0200

    fix refresh strategy

--- src/modules/auth/strategies/refresh.strategy.ts
+++ src/modules/auth/strategies/refresh.strategy.ts
@@ -5,7 +5,10 @@ import { TokenPayload } from '../interfaces/token-payload.interface';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class JwtRefreshStrategy extends PassportStrategy(Strategy) {
+export class JwtRefreshStrategy extends PassportStrategy(
+  Strategy,
+  'jwt-refresh',
+) {
   constructor(private readonly configService: ConfigService) {
     const secret = configService.get<string>('JWT_REFRESH_SECRET');
     super({
